4f7a91c0e3765ae6cf8ae7eed3def28f639d4e05
sudharsanbaskaran09-eng <sudharsanbaskaran09@gmail.com>
2026-01-23 14:47:18 +0530

    Update publisher_lambda.py

--- 02-Serverless-And-Modern-Web-Hosting/project-06-Event-Driven-Microservices/publisher_lambda.py
+++ 02-Serverless-And-Modern-Web-Hosting/project-06-Event-Driven-Microservices/publisher_lambda.py
@@ -1,1 +1,28 @@
+import json
+import boto3
+
+sns = boto3.client('sns')
+
+# Replace with your actual SNS Topic ARN
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:191280371533:event-driven-topic"
+
+def lambda_handler(event, context):
+    message = {
+        "eventType": "ORDER_CREATED",
+        "orderId": "ORD-1001",
+        "status": "CREATED"
+    }
+
+    response = sns.publish(
+        TopicArn=SNS_TOPIC_ARN,
+        Message=json.dumps(message)
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Event published successfully",
+            "messageId": response["MessageId"]
+        })
+    }
 
